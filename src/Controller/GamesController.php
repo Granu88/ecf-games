c43f7d0c3a9c6037ba92332cdae34e456f5e523d
David <davidvallon@hotmail.fr>
2020-01-14 14:35:18 +0100

    pagination

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 use App\Entity\Game;
 
 class GamesController extends AbstractController
@@ -11,16 +13,21 @@ class GamesController extends AbstractController
     /**
      * @Route("/", name="games")
      */
-    public function index()
+    public function index(Request $request, PaginatorInterface $paginator)
     {
       $games = $this->getDoctrine()
-      ->getRepository(Game::class)
-      ->findBy([],['name'=>'ASC']);
+        ->getRepository(Game::class)
+        ->findBy([],['name'=>'ASC']);
 
-    
+      $gamesList = $paginator->paginate(
+        $games,
+        $request->query->getInt('page', 1),
+        20
+      );
 
       return $this->render('games/index.html.twig', [
           'games' => $games,
+          'gamesList' => $gamesList,
       ]);
     }
 
